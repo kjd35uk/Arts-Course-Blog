dc6b8a64b70eca394cee29bb891a36375a1729ae
Kirstie Davidson <davidson.kirstiej@gmail.com>
2021-04-12 16:50:18 +0100

    Add blog template styling

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -2,16 +2,19 @@ import React from 'react';
 import { graphql, Link } from 'gatsby';
 
 import Layout from '../components/Layout';
+import './blogTemplate.css';
 
 export default function Template({ data }) {
   const post = data.markdownRemark;
   const { title, author, date } = post.frontmatter;
   return (
     <Layout>
-      <Link to="/">Back to blogs</Link>
-      <h1>{title}</h1>
-      <p>Posted by {author} on {date}</p>
-      <div dangerouslySetInnerHTML={{ __html: post.html }}/>
+      <div className="blogTemplate">
+        <Link to="/blog">Back to blogs</Link>
+        <h1 className="blogTemplate-title">{title}</h1>
+        <p className="blogTemplate-posted-by">Posted by {author} on {date}</p>
+        <div dangerouslySetInnerHTML={{ __html: post.html }}/>
+      </div>
     </Layout>
   )
 };
